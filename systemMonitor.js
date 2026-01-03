531de43ea22db85aebe8bf23ad4d7c2ef00e4d8a
ANK <ankury114112118@gmail.com>
2026-01-03 19:16:00 +0530

    init npm and installed chalk

--- systemMonitor.js
+++ systemMonitor.js
@@ -1,4 +1,4 @@
-import os from 'node:os';
+import os from "node:os";
 
 //console.log("CPU",os.cpus());
 // console.log("CPU",os.cpus().length);
@@ -6,42 +6,40 @@ import os from 'node:os';
 // console.log("Free Memory", os.freemem()/(1024*1024*1024),"GB");
 
 // console.log("Up time ", os.uptime()/(60*60)); //hours
-// console.log("Host namme : ", os.hostname()); 
-// console.log("User Info : ", os.userInfo()); 
-// console.log("Machine : ", os.machine()); 
+// console.log("Host namme : ", os.hostname());
+// console.log("User Info : ", os.userInfo());
+// console.log("Machine : ", os.machine());
 
-function monitor (){
-    //take a snapshot
-    //take another snapshot after a second
+function monitor() {
+  //take a snapshot
+  //take another snapshot after a second
 
-    const oldCpu = os.cpus()
-    setTimeout(()=>{
-       const newCpus = os.cpus();
-        const usage = newCpus.map((cpu,i)=>{
-            return{
-                core: i,
-                usage: calculateCpu(oldCpu[i],newCpus[i])+'%',
-
-            }
-            
-        })
-        console.clear();
-       console.table(usage);
-        const usedMemory = (os.totalmem()- os.freemem() )/(1024*1024*1024)
-       console.log(`
-            Memory used : ${usedMemory.toFixed(2)} GB / ${(os.totalmem()/(1024*1024*1024)).toFixed(2)} GB
-        `)
-
-    },1000)
-    
+  const oldCpu = os.cpus();
+  setTimeout(() => {
+    const newCpus = os.cpus();
+    const usage = newCpus.map((cpu, i) => {
+      return {
+        core: i,
+        usage: calculateCpu(oldCpu[i], newCpus[i]) + "%",
+      };
+    });
+    console.clear();
+    console.table(usage);
+    const usedMemory = (os.totalmem() - os.freemem()) / (1024 * 1024 * 1024);
+    console.log(
+      `Memory used : ${usedMemory.toFixed(2)} GB / ${(
+        os.totalmem() /
+        (1024 * 1024 * 1024)
+      ).toFixed(2)} GB`
+    );
+  }, 1000);
 }
-function calculateCpu(oldCpu, newCpus){
-    const oldTotal = Object.values(oldCpu.times).reduce((a,b)=>a+b);
-    const newTotal = Object.values(newCpus.times).reduce((a,b)=>a+b);
-    const idle = newCpus.times.idle - oldCpu.times.idle
-    const total = newTotal - oldTotal;
-    const used = total - idle;
-    return ((100*used)/total).toFixed(1);
-
+function calculateCpu(oldCpu, newCpus) {
+  const oldTotal = Object.values(oldCpu.times).reduce((a, b) => a + b);
+  const newTotal = Object.values(newCpus.times).reduce((a, b) => a + b);
+  const idle = newCpus.times.idle - oldCpu.times.idle;
+  const total = newTotal - oldTotal;
+  const used = total - idle;
+  return ((100 * used) / total).toFixed(1);
 }
-setInterval(monitor,1000)
+setInterval(monitor, 1000);
